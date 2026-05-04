077a26cef5d06aff8850ff649056fb0a1f8e610e
jlbuild <juliabuildbot@gmail.com>
2026-05-04 02:03:41 +0000

    libsigcpp_jll build 3.6.0+0

--- src/libsigcpp_jll.jl
+++ src/libsigcpp_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libsigcpp")
-JLLWrappers.@generate_main_file("libsigcpp", UUID("8855df87-3ca1-5a3d-a68b-4f0ddc198ba8"))
+JLLWrappers.@generate_main_file("libsigcpp", Base.UUID("8855df87-3ca1-5a3d-a68b-4f0ddc198ba8"))
 end  # module libsigcpp_jll
